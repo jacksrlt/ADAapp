5c54df1c41d52ffc735c1ad720cb42480e86d2f1
Jack <40079880+jacksrlt@users.noreply.github.com>
2022-02-15 12:11:00 +0100

    Working firebase realtime database recycler

---
+++ app/src/main/java/com/mijale/adaapp/Fragments/AdapterListas.java
@@ -0,0 +1,77 @@
+package com.mijale.adaapp.Fragments;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.mijale.adaapp.R;
+
+import java.util.List;
+
+public class AdapterListas extends RecyclerView.Adapter<AdapterListas.MyViewHolder> {
+
+    public List<Post> post;
+    private RelativeLayout linearLayout;
+    Context context;
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView user, content;
+        ImageView avatar;
+
+        public MyViewHolder(View view) {
+            super(view);
+            user = (TextView) view.findViewById(R.id.nombreuser);
+            content = view.findViewById(R.id.textodentro);
+            avatar = view.findViewById(R.id.imagen);
+            linearLayout = view.findViewById(R.id.clickRecyclerView);
+        }
+    }
+
+    public AdapterListas(Context context, List<Post> post) {
+        this.post = post;
+        this.context = context;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.lista, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        Post recycleList = post.get(position);
+        holder.user.setText(recycleList.getUseruid());
+        holder.content.setText(recycleList.getMessage());
+        Glide.with(context).load(recycleList.getImage()).into(holder.avatar);
+
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Post recycleList = post.get(position);
+                String name = recycleList.getUseruid();
+                Toast.makeText(context, "User Name : " + name, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return post.size();
+    }
+
+}
--- app/src/main/java/com/mijale/adaapp/Fragments/ForumFragment.java
+++ app/src/main/java/com/mijale/adaapp/Fragments/ForumFragment.java
@@ -1,64 +1,115 @@
 package com.mijale.adaapp.Fragments;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-import com.mijale.adaapp.Adapters.AdapterListas;
-import com.mijale.adaapp.Entidades.Persona;
-import com.mijale.adaapp.PerfilActivity;
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mijale.adaapp.R;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 
 public class ForumFragment extends Fragment {
 
+    private DatabaseReference mDatabase;
     AdapterListas adapterListas;
-    RecyclerView recyclerViewPersonas;
-    ArrayList <Persona> listaPersonas;
+    private RecyclerView recyclerViewPersonas;
+    ArrayList <Post> listaPersonas = new ArrayList<>();;
+
+    String mProfileImageUrl;
+    ProgressDialog progressDialog;
+    String user = null, message= null;
+
     public ForumFragment() {
-        // Required empty public constructor
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
+        mDatabase = FirebaseDatabase.getInstance("https://adaapp-f73d9-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Messages");
         View view =  inflater.inflate(R.layout.fragment_forum, container, false);
-        recyclerViewPersonas = view.findViewById(R.id.recycler);
-        listaPersonas = new ArrayList<>();
-        cargarLista();
+        recyclerViewPersonas = view.findViewById(R.id.recyclerPost);
         mostrarData();
-    return view;
+        cargarLista();
+        dialogoProgreso();
+        return view;
     }
 
-    public void cargarLista(){
-        listaPersonas.add(new Persona("User","hola!!",R.drawable.common_google_signin_btn_icon_dark_normal_background));
-        listaPersonas.add(new Persona("User2","holaaa!!",R.drawable.common_google_signin_btn_icon_dark_normal));
-        listaPersonas.add(new Persona("User","hola!!",R.drawable.common_google_signin_btn_icon_dark_normal_background));
-        listaPersonas.add(new Persona("User2","holaaa!!",R.drawable.common_google_signin_btn_icon_dark_normal));
-        listaPersonas.add(new Persona("User","hola!!",R.drawable.common_google_signin_btn_icon_dark_normal_background));
-        listaPersonas.add(new Persona("User2","holaaa!!",R.drawable.common_google_signin_btn_icon_dark_normal));
-        listaPersonas.add(new Persona("User","hola!!",R.drawable.common_google_signin_btn_icon_dark_normal_background));
-        listaPersonas.add(new Persona("User2","holaaa!!",R.drawable.common_google_signin_btn_icon_dark_normal));
-        listaPersonas.add(new Persona("User","hola!!",R.drawable.common_google_signin_btn_icon_dark_normal_background));
-        listaPersonas.add(new Persona("User2","holaaa!!",R.drawable.common_google_signin_btn_icon_dark_normal));
+    private void cargarLista(){
+        listaPersonas.clear();
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                allListData(dataSnapshot);
+                adapterListas.notifyDataSetChanged();
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
+    }
+
+    @SuppressLint("NewApi")
+    public void allListData(final DataSnapshot dataSnapshot){
+        if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0) {
+
+            Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+            if (map.get("useruid") != null) {
+                user = map.get("useruid").toString();
+            }
+            if (map.get("message") != null) {
+                message = map.get("message").toString();
+            }
+            if (map.get("image") != null){
+                mProfileImageUrl = map.get("image").toString();
+            }
+        }
+
+        progressDialog.dismiss();
+
+        listaPersonas.add(new Post(user,message,mProfileImageUrl));
     }
 
     public void mostrarData(){
         recyclerViewPersonas.setLayoutManager(new LinearLayoutManager(getContext()));
         adapterListas = new AdapterListas(getContext(), listaPersonas);
+        recyclerViewPersonas.setItemAnimator(new DefaultItemAnimator());
         recyclerViewPersonas.setAdapter(adapterListas);
     }
-}
 
+    public void dialogoProgreso() {
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading....");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
+        progressDialog.setIndeterminate(false);
+        progressDialog.show();
+    }
+}
