ed0cc76e5f0fbeeacf074d883ba06bb38d0fa4e5
Jack <40079880+jacksrlt@users.noreply.github.com>
2022-02-21 13:44:52 +0100

    Working firebase realtime database recycler (2)

--- app/src/main/java/com/mijale/adaapp/Fragments/ForumFragment.java
+++ app/src/main/java/com/mijale/adaapp/Fragments/ForumFragment.java
@@ -4,37 +4,50 @@ import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mijale.adaapp.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 
 public class ForumFragment extends Fragment {
 
     private DatabaseReference postDatabase;
-    //private DatabaseReference userDatabase;
+
     AdapterListas adapterListas;
     private RecyclerView recyclerViewPersonas;
-    ArrayList <Post> listaPersonas = new ArrayList<>();;
+    ArrayList<Post> listaPersonas = new ArrayList<>();
+    FirebaseFirestore userStore;
 
-    String mProfileImageUrl;
+    String imageRef;
     ProgressDialog progressDialog;
-    String user = null, message= null;
+    String userRef = null, message = null;
 
     public ForumFragment() {
     }
@@ -43,7 +56,8 @@ public class ForumFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         postDatabase = FirebaseDatabase.getInstance("https://adaapp-f73d9-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Messages");
-        View view =  inflater.inflate(R.layout.fragment_forum, container, false);
+        userStore = FirebaseFirestore.getInstance();
+        View view = inflater.inflate(R.layout.fragment_forum, container, false);
         recyclerViewPersonas = view.findViewById(R.id.recyclerPost);
         mostrarData();
         cargarLista();
@@ -51,7 +65,7 @@ public class ForumFragment extends Fragment {
         return view;
     }
 
-    private void cargarLista(){
+    private void cargarLista() {
         listaPersonas.clear();
         postDatabase.addChildEventListener(new ChildEventListener() {
             @Override
@@ -59,46 +73,51 @@ public class ForumFragment extends Fragment {
                 allListData(dataSnapshot);
                 adapterListas.notifyDataSetChanged();
             }
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
 
             @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
 
             @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) { }
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
         });
 
     }
 
     @SuppressLint("NewApi")
-    public void allListData(final DataSnapshot dataSnapshot){
-        if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0) {
+    public void allListData(final DataSnapshot dataSnapshot) {
+        if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
 
             Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
             if (map.get("useruid") != null) {
-                user = map.get("useruid").toString();
+                userRef = map.get("useruid").toString();
             }
             if (map.get("message") != null) {
                 message = map.get("message").toString();
             }
-            if (map.get("image") != null){
-                mProfileImageUrl = map.get("image").toString();
+            if (map.get("image") != null) {
+                imageRef = map.get("message").toString();;
             }
         }
 
         progressDialog.dismiss();
 
-        listaPersonas.add(new Post(user,message,mProfileImageUrl));
+        listaPersonas.add(new Post(userRef, message, imageRef));
     }
 
-    public void mostrarData(){
+    public void mostrarData() {
         recyclerViewPersonas.setLayoutManager(new LinearLayoutManager(getContext()));
         adapterListas = new AdapterListas(getContext(), listaPersonas);
-        recyclerViewPersonas.setItemAnimator(new DefaultItemAnimator());
         recyclerViewPersonas.setAdapter(adapterListas);
     }
 
@@ -110,5 +129,6 @@ public class ForumFragment extends Fragment {
         progressDialog.setIndeterminate(false);
         progressDialog.show();
     }
+
 
 }
--- app/src/main/java/com/mijale/adaapp/LoginActivity.java
+++ app/src/main/java/com/mijale/adaapp/LoginActivity.java
@@ -66,8 +66,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                loginAnon();
             }
         });
     }
