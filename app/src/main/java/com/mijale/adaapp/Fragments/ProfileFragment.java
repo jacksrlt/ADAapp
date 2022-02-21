5415092dad0dc5adac7197c0e1bcd78c2fd2ca4e
barrigamiguelm <barrigamiguelm@gmail.com>
2022-02-21 14:00:51 +0100

    "mas o menos"

--- app/src/main/java/com/mijale/adaapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/mijale/adaapp/Fragments/ProfileFragment.java
@@ -1,17 +1,39 @@
 package com.mijale.adaapp.Fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 import com.mijale.adaapp.R;
 
 public class ProfileFragment extends Fragment {
 
+
+    private static final int CAMERA_REQUEST = 1888;
+    public static final int PICK_IMAGE = 1;
+    Button btSubirImg;
+    ImageView circleImageView;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -21,6 +43,69 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btSubirImg = view.findViewById(R.id.btSubirImg);
+        circleImageView = view.findViewById(R.id.circleImageView);
+
+        btSubirImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog ad = new AlertDialog.Builder(getContext())
+                        .create();
+                ad.setCancelable(false);
+                ad.setTitle("title");
+                ad.setButton("Abir Camara", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        abirCamara();
+                    }
+                });
+                ad.setButton2("Abrir Galeria", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        abrirGaleria();
+                    }
+                });
+
+
+                ad.show();
+
+            }
+        });
+
+
+        return view;
+
+
     }
+
+    private void abirCamara() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 0:
+                if (resultCode == RESULT_OK) {
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    circleImageView.setImageBitmap(photo);
+
+                }
+
+
+        }
+    }
+
+
+    private void abrirGaleria() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
+
 }
