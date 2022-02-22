0bc1ebdc3cdd762e4ebb3ef70ceaa7c1d73574bd
barrigamiguelm <barrigamiguelm@gmail.com>
2022-02-22 09:01:17 +0100

    "Ahora puedes poner foto de perfil"

--- app/src/main/java/com/mijale/adaapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/mijale/adaapp/Fragments/ProfileFragment.java
@@ -1,17 +1,42 @@
 package com.mijale.adaapp.Fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 import com.mijale.adaapp.R;
 
+import java.io.IOException;
+
 public class ProfileFragment extends Fragment {
 
+
+    private static final int CAMERA_REQUEST = 1888;
+    public static final int PICK_IMAGE = 1;
+    public static final int GALLERY_REQUEST = 1;
+    Button btSubirImg;
+    ImageView circleImageView;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -21,6 +46,73 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btSubirImg = view.findViewById(R.id.btSubirImg);
+        circleImageView = view.findViewById(R.id.circleImageView);
+
+        btSubirImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog ad = new AlertDialog.Builder(getContext())
+                        .create();
+                ad.setCancelable(false);
+                ad.setTitle("Foto de perfil");
+                ad.setButton("Camara", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        abirCamara();
+                    }
+                });
+                ad.setButton2("Galeria", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        abrirGaleria();
+                    }
+                });
+
+
+                ad.show();
+
+            }
+        });
+
+
+        return view;
+
+
     }
+
+    private void abirCamara() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode) {
+                case GALLERY_REQUEST:
+                    Uri selectedImage;
+                    selectedImage = data.getData();
+                    circleImageView.setImageURI(selectedImage);
+                    break;
+                case CAMERA_REQUEST:
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    circleImageView.setImageBitmap(photo);
+            }
+    }
+
+
+    private void abrirGaleria() {
+
+        Intent gallery = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+
+
+    }
+
+
 }
