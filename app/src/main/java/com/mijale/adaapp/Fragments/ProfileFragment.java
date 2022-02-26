13078966e0216eb1f64b960147d1a5db86286e1c
Miguel <barrigamiguelm@gmai.com>
2022-02-26 13:50:44 +0100

    "Perfil terminado"

--- app/src/main/java/com/mijale/adaapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/mijale/adaapp/Fragments/ProfileFragment.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultCallback;
@@ -18,7 +19,9 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,17 +39,20 @@ import com.mijale.adaapp.R;
 import com.mijale.adaapp.SplashScreen;
 
 import java.io.IOException;
+import java.sql.Struct;
 
 public class ProfileFragment extends Fragment {
 
 
-//    private static final int CAMERA_REQUEST = 1888;
-//    public static final int PICK_IMAGE = 1;
-//    public static final int GALLERY_REQUEST = 1;
-    private TextView tvNombre,tvPais,tvCorreo,tvTelefono,tvOficio,tvLink;
-    private String nombre,pais,link,correo,oficio,telefono;
+    private static final int CAMERA_REQUEST = 1888;
+    public static final int PICK_IMAGE = 1;
+    public static final int GALLERY_REQUEST = 1;
+    SharedPreferences.Editor editor;
+    SharedPreferences preferences;
+    private TextView tvNombre, tvPais, tvCorreo, tvTelefono, tvOficio, tvLink;
+    private String nombre, pais, link, correo, oficio, telefono;
     private ImageView circleImageView;
-    private ImageButton Editar;
+    private ImageButton btEdit;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -56,43 +62,31 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        Editar=view.findViewById(R.id.btEdit);
+        preferences = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
 
-//        View contexto= view ;
-//        SharedPreferences shpref = getActivity().getPreferences(contexto.MODE_PRIVATE);
-        //btguardar=view.findViewById(R.id.btGuardar);
-        //btSubirImg = view.findViewById(R.id.btSubirImg);
+
+
+        btEdit = view.findViewById(R.id.btEdit);
         circleImageView = view.findViewById(R.id.circleImageView);
-        tvNombre=view.findViewById(R.id.tvNombre);
-        tvPais=view.findViewById(R.id.tvPais);
-        tvCorreo=view.findViewById(R.id.tvCorreo);
-        tvTelefono=view.findViewById(R.id.tvTelefono);
-        tvOficio=view.findViewById(R.id.tvOficio);
-        tvLink=view.findViewById(R.id.tvLink);
+        tvNombre = view.findViewById(R.id.tvNombre);
+        tvPais = view.findViewById(R.id.tvPais);
+        tvCorreo = view.findViewById(R.id.tvCorreo);
+        tvTelefono = view.findViewById(R.id.tvTelefono);
+        tvOficio = view.findViewById(R.id.tvOficio);
+        tvLink = view.findViewById(R.id.tvLink);
+        load();
 
 
-//// esto es para inte
-//        SharedPreferences shpref =getPreferences(context.MODE_PRIVATE);
-//        nombre=shpref.getString("datoNombre","No hay dato");
-//        pais=shpref.getString("datoPais","No hay dato");
-//        correo=shpref.getString("datoCorreo","No hay dato");
-//        telefono=shpref.getString("datoTelefono","No hay dato");
-//        oficio=shpref.getString("datoOficio","No hay dato");
-//        link=shpref.getString("datoLink","No hay dato");
 
-        tvNombre.getText(nombre);
-        tvPais.getText(pais);
-        tvCorreo.getText(correo);
-        tvTelefono.getText(telefono);
-        tvOficio.getText(oficio);
-        tvLink.getText(link);
 
 
 
 
         // onclick para ir a perfil
-        Editar.setOnClickListener(new View.OnClickListener() {
+        btEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), Prefil.class);
@@ -100,68 +94,27 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-//        btSubirImg.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                AlertDialog ad = new AlertDialog.Builder(getContext())
-//                        .create();
-//                ad.setCancelable(false);
-//                ad.setTitle("Foto de perfil");
-//                ad.setButton("Camara", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialogInterface, int i) {
-//                        abirCamara();
-//                    }
-//                });
-//                ad.setButton2("Galeria", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialogInterface, int i) {
-//                        abrirGaleria();
-//                    }
-//                });
-//
-//
-//                ad.show();
-//
-//            }
-//        });
-
 
         return view;
 
-
     }
 
-//    private void abirCamara() {
-//        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-//        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-//    }
-//
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (resultCode == Activity.RESULT_OK)
-//            switch (requestCode) {
-//                case GALLERY_REQUEST:
-//                    Uri selectedImage;
-//                    selectedImage = data.getData();
-//                    circleImageView.setImageURI(selectedImage);
-//                    break;
-//                case CAMERA_REQUEST:
-//                    Bitmap photo = (Bitmap) data.getExtras().get("data");
-//                    circleImageView.setImageBitmap(photo);
-//            }
-//    }
-//
-//
-//    private void abrirGaleria() {
-//
-//        Intent gallery = new Intent(Intent.ACTION_PICK,
-//                MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-//        startActivityForResult(gallery, PICK_IMAGE);
-//
-//
-//    }
+
+    public void load(){
+        String nombre = preferences.getString("nombre", "SIN DATOS");
+        String pais = preferences.getString("pais", "SIN DATOS");
+        String correo = preferences.getString("correo", "SIN DATOS");
+        String telefono = preferences.getString("telefono", "xxx xxx xxx");
+        String oficio = preferences.getString("oficio", "SIN DATOS");
+        String gitHub = preferences.getString("gitHub", "SIN DATOS");
+
+        tvNombre.setText(nombre);
+        tvPais.setText(pais);
+        tvCorreo.setText(correo);
+        tvTelefono.setText(telefono);
+        tvOficio.setText(oficio);
+        tvLink.setText(gitHub);
+    }
 
 
 }
--- app/src/main/java/com/mijale/adaapp/Prefil.java
+++ app/src/main/java/com/mijale/adaapp/Prefil.java
@@ -1,15 +1,18 @@
 package com.mijale.adaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -17,55 +20,58 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.mijale.adaapp.Fragments.ProfileFragment;
+
 public class Prefil extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     public static final int PICK_IMAGE = 1;
     public static final int GALLERY_REQUEST = 1;
-    private Button btSubirImg,btguardar;
+    private Button btSubirImg, btguardar;
     private ImageView circleImageView;
-    private EditText etNombre,etPais,etTelefono,etCorreo,etOficio,etLink;
+    SharedPreferences.Editor editor;
+    SharedPreferences preferences;
+    private TextInputEditText etNombre, etPais, etTelefono, etCorreo, etOficio, etGithub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prefil);
 
-        Context context=this;
-        SharedPreferences shpref = getSharedPreferences("pref",context.MODE_PRIVATE);
+        etOficio = findViewById(R.id.etOficio);
+        etGithub = findViewById(R.id.etGithub);
+        etTelefono = findViewById(R.id.etTelefono);
+        etCorreo = findViewById(R.id.etCorreo);
+        etNombre = findViewById(R.id.etNombre);
+        etPais = findViewById(R.id.etPais);
+        btguardar = findViewById(R.id.btGuardar);
+        btSubirImg = findViewById(R.id.btSubirImg);
 
-        etOficio=findViewById(R.id.etOficio);
-        etLink=findViewById(R.id.etlink);
-        etTelefono=findViewById(R.id.etTelefono);
-        etCorreo=findViewById(R.id.etCorreo);
-        etNombre=findViewById(R.id.etNombre);
-        etPais=findViewById(R.id.etPais);
-        btguardar=findViewById(R.id.btGuardar);
-        //metodo para guardar los datos
+
+        preferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+
+
+
         btguardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences shpref =getPreferences(context.MODE_PRIVATE);
-                SharedPreferences.Editor editor=shpref.edit();
-                editor.putString("datoNombre",etNombre.getText().toString());
-                editor.putString("datoPais",etPais.getText().toString());
-                editor.putString("datoCorreo",etCorreo.getText().toString());
-                editor.putString("datoTelefono",etTelefono.getText().toString());
-                editor.putString("datoOficio",etOficio.getText().toString());
-                editor.putString("datoLink",etLink.getText().toString());
-                editor.commit();
+                guardarDatos();
+                onBackPressed();
 
             }
+
+
         });
-        btSubirImg =findViewById(R.id.btSubirImg);
-        circleImageView =findViewById(R.id.circleImageView);
 
         btSubirImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder ad = new AlertDialog.Builder(this)
+                AlertDialog ad = new AlertDialog.Builder(Prefil.this)
                         .create();
                 ad.setCancelable(false);
-                ad.setTitle("Foto de perfil");
+                ad.setTitle("Fuente de imagen");
                 ad.setButton("Camara", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -85,16 +91,14 @@ public class Prefil extends AppCompatActivity {
             }
         });
 
-
-        return context;
-
-
-
     }
+
+
     private void abirCamara() {
         Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -103,13 +107,17 @@ public class Prefil extends AppCompatActivity {
                 case GALLERY_REQUEST:
                     Uri selectedImage;
                     selectedImage = data.getData();
+
                     circleImageView.setImageURI(selectedImage);
                     break;
                 case CAMERA_REQUEST:
                     Bitmap photo = (Bitmap) data.getExtras().get("data");
                     circleImageView.setImageBitmap(photo);
             }
+
     }
+
+
     private void abrirGaleria() {
 
         Intent gallery = new Intent(Intent.ACTION_PICK,
@@ -118,4 +126,27 @@ public class Prefil extends AppCompatActivity {
 
 
     }
+
+    private void guardarDatos() {
+        SharedPreferences.Editor editor = preferences.edit();
+        String nombre = etNombre.getText().toString();
+        String pais = etPais.getText().toString();
+        String correo = etCorreo.getText().toString();
+        String telefono = etTelefono.getText().toString();
+        String oficio = etOficio.getText().toString();
+        String gitHub = etGithub.getText().toString();
+        editor.putString("nombre","@"+ nombre);
+        editor.putString("pais", "Pais:  "+pais);
+        editor.putString("correo", "Correo: " + correo);
+        editor.putString("telefono","Telefono:" +telefono);
+        editor.putString("oficio","Trabaja en : "+ oficio);
+        editor.putString("gitHub", "Github: "+ gitHub);
+        editor.commit();
+    }
+
+    private void lanzar() {
+        Intent intent = new Intent(this, ProfileFragment.class);
+        startActivity(intent);
+    }
+
 }
