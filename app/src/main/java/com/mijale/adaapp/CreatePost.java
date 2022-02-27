90e8715473ef2685178fdd6a7c78caa76a18ad7a
Jack <40079880+jacksrlt@users.noreply.github.com>
2022-02-27 02:19:16 +0100

    Post created and sent to Realtime Database

--- app/src/main/java/com/mijale/adaapp/CreatePost.java
+++ app/src/main/java/com/mijale/adaapp/CreatePost.java
@@ -1,20 +1,65 @@
 package com.mijale.adaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.mijale.adaapp.Fragments.Post;
 
 public class CreatePost extends AppCompatActivity {
 
-    FloatingActionButton fab;
+    private Button btEnviarPost;
+    private EditText etPost;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore fStore;
+    String message, useruid, userID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getInstance("https://adaapp-f73d9-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Messages");
+        mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        btEnviarPost = findViewById(R.id.btEnviarPost);
+        etPost = findViewById(R.id.etPost);
+
+
+
+        btEnviarPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                useruid = mAuth.getCurrentUser().getUid();
+                message = etPost.getText().toString();
+                SendPost sendPost = new SendPost(useruid, message);
+
+                databaseReference.push().setValue(sendPost);
+            }
+        });
 
 
     }
