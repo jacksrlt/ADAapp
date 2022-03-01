10d72880f1b482a2cebc27c968d90ecd6c8c02e1
Miguel <barrigamiguelm@gmai.com>
2022-03-01 14:51:20 +0100

    "Fix"

--- app/src/main/java/com/mijale/adaapp/SplashScreen.java
+++ app/src/main/java/com/mijale/adaapp/SplashScreen.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         openApp(true);
 
